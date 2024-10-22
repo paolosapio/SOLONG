996aa7dc675fb7dff2803aa7eaef70e0712f1420
Paolo Sapio <psapio@c1r17s5.42madrid.com>
2024-10-22 16:12:12 +0200

    lo tenemos

--- animation_next_texture.c
+++ animation_next_texture.c
@@ -9,7 +9,6 @@ void animation_next_texture(t_animation *animation)
 	if (animation->current_texture == animation->total_texture)
 		animation->current_texture = 0;
 	animation->image->pixels = animation->textures[animation->current_texture]->pixels;
-
 	animation->current_texture++;
 }
 // con ft_memmove seria asi (lo de antes)
